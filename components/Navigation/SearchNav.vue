428e5def6e3002e5614d0f35942de2e5cf451d08
NV7150 <7150macnv777dai@gmail.com>
2022-03-12 14:20:31 +0900

    rebuild

---
+++ components/Navigation/SearchNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-navigation-drawer
+    v-model="rightDrawer"
+    temporary
+    fixed
+    right
+  >
+    <v-list>
+
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from "nuxt-property-decorator";
+
+@Component
+export default class SearchNav extends  Vue{
+  @Prop({type: Boolean, required: true})
+  rightDrawer!: boolean;
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
